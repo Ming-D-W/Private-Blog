4a165a8ac9af8e9df2fc57d412c474d1aa654d84
Ming-D-W <wuwanming0405@163.com>
2025-01-28 23:01:51 +0800

    遍历目录生成路由

--- src/components/layout/components/Main.vue
+++ src/components/layout/components/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main" style="background-color: #fcf85b">
+  <div class="main">
     <router-view></router-view>
   </div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,29 @@
 const { defineConfig } = require("@vue/cli-service");
+const markdownContainer = require("./src/plugins/markdownContainer.js");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.module
+      .rule("md")
+      .test(/\.md$/)
+      .use("vue-loader")
+      .loader("vue-loader")
+      .end()
+      .use("vue-markdown-loader")
+      .loader("vue-markdown-loader/lib/markdown-compiler")
+      .options({
+        raw: true,
+        preventExtract: true,
+        use: [
+          [
+            require("markdown-it-container"),
+            "demo",
+            markdownContainer, // 引入外部配置文件中的配置项
+          ],
+        ],
+      });
+  },
   css: {
     loaderOptions: {
       scss: {
